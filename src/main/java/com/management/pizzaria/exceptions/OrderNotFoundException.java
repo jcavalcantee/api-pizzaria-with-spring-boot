046a45dab2940bdbed6d09a58bcaae33118cdcba
jcavalcantee <jcavalcantes@gmail.com>
2024-02-18 19:40:31 -0300

    create class to return an exception if a order is not found

---
+++ src/main/java/com/management/pizzaria/exceptions/OrderNotFoundException.java
@@ -0,0 +1,8 @@
+package com.management.pizzaria.exceptions;
+
+public class OrderNotFoundException extends Exception {
+
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
+}
