e289e39297c3b83e3307f4f237b631b1c776fd0b
jcavalcantee <jcavalcantes@gmail.com>
2024-02-18 18:31:20 -0300

    create method to find drink by name on Service

--- src/main/java/com/management/pizzaria/services/DrinkService.java
+++ src/main/java/com/management/pizzaria/services/DrinkService.java
@@ -1,6 +1,7 @@
 package com.management.pizzaria.services;
 
 import com.management.pizzaria.dtos.DrinkDTO;
+import com.management.pizzaria.exceptions.ProductNotFoundException;
 import com.management.pizzaria.models.Drink;
 import com.management.pizzaria.repositories.DrinkRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DrinkService extends ProductService{
@@ -32,6 +34,15 @@ public class DrinkService extends ProductService{
 
     public Drink getDrinkById(Long id) throws Exception {
         return this.drinkRepository.findById(id).orElseThrow(() -> new Exception("Drink with ID provided not found"));
+    }
+
+    public Drink getDrinkByName(String name) throws ProductNotFoundException {
+        Optional<Drink> drinkOptional = Optional.ofNullable(this.drinkRepository.findByName(name));
+        if (drinkOptional.isPresent()) {
+            return drinkOptional.get();
+        } else {
+            throw new ProductNotFoundException("Drink with name provided not found");
+        }
     }
 
     public Drink updateDrink(Long id, Drink drink) throws Exception {
