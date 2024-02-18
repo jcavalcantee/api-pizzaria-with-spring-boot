2ffdc858ef91554d17f1721b2fd9f1fcbe0aa8e8
jcavalcantee <jcavalcantes@gmail.com>
2024-02-18 18:35:07 -0300

    create class to return an exception if a customer is not found

---
+++ src/main/java/com/management/pizzaria/exceptions/CustomerNotFoundException.java
@@ -0,0 +1,7 @@
+package com.management.pizzaria.exceptions;
+
+public class CustomerNotFoundException extends Exception {
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
+}
