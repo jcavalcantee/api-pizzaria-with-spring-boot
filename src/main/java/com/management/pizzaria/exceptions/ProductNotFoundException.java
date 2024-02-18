a7bb263f5e787278478de846ed1e10151f4b75c4
jcavalcantee <jcavalcantes@gmail.com>
2024-02-18 18:35:16 -0300

    create class to return an exception if a product is not found

---
+++ src/main/java/com/management/pizzaria/exceptions/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package com.management.pizzaria.exceptions;
+
+public class ProductNotFoundException extends Exception {
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+}
